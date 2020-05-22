d93feaf3dffa85cd8654b08de8a0bb800c4d0ace
ethanshearer123 <62487806+ethanshearer123@users.noreply.github.com>
2020-05-22 00:11:07 -0400

    Add files via upload

---
+++ rp_afkkick/client.lua
@@ -0,0 +1,50 @@
+-- Idea and based off Scammer's AFK resource | by ethanol20#4434
+-- https://forum.cfx.re/t/release-afk-and-high-ping-kick/7904
+-- Join my Discord for more awesome scripts and to submit your ideas so i can make them!
+-- https://discord.gg/SQXvn5a
+
+
+local kicktime = 600 --(IN SECONDS)
+local voice = false
+
+Citizen.CreateThread(function()	
+	while true do
+		Wait(1000)
+		playerPed = GetPlayerPed(-1)
+		if playerPed then
+			currentPos = GetEntityCoords(playerPed, true)
+			if currentPos == prevPos and not IsEntityDead(playerPed) then
+				if time > 0 then
+					if time == math.ceil(kicktime / 5) then
+						ShowInfo("You will be kicked in " .. time .. " seconds")
+					end
+					if time == math.ceil(kicktime / 4) then
+						ShowInfo("You will be kicked in " .. time .. " seconds")
+					end
+					if time == math.ceil(kicktime / 3) then
+						ShowInfo("You will be kicked in " .. time .. " seconds")
+					end
+					if time == math.ceil(kicktime / 2) then
+						ShowInfo("You will be kicked in " .. time .. " seconds")
+					end
+					time = time - 1
+				else
+					TriggerServerEvent("afk:kick")
+				end
+			else
+				time = kicktime
+			end
+			if time == math.ceil(kicktime / 5) then
+				TriggerServerEvent('InteractSound_SV:PlayOnAll', 'afk', 0.8)
+			end
+			prevPos = currentPos
+		end
+	end
+end)
+
+function ShowInfo(text)
+    SetNotificationTextEntry("STRING");
+    AddTextComponentString(text);
+	SetNotificationMessage('CHAR_FLOYD', 'AFK', true, 1, '[Project X] AFK KICK');
+	DrawNotification(false, false)
+end 
---
+++ rp_afkkick/server.lua
@@ -0,0 +1,34 @@
+-- Idea and based off Scammer's AFK resource | by ethanol20#4434
+-- https://forum.cfx.re/t/release-afk-and-high-ping-kick/7904
+-- Join my Discord for more awesome scripts and to submit your ideas so i can make them!
+-- https://discord.gg/SQXvn5a
+
+
+local reason = '[Project X] You have been AFK for too long. Bandwidth is not CHEAPPPPP. K thx baye.'
+local webhook = 'WEBHOOKHERE'
+local loggedalready = false -- Set this to true if you have a script that handles this already.
+
+RegisterNetEvent('afk:kick')
+AddEventHandler('afk:kick', function(player)
+    local name = GetPlayerName(source)
+    if loggedalready == false and not IsPlayerAceAllowed(source, 'afk:immune') then
+        DropPlayer(source, reason)
+        DiscordWebHook(name, reason, color)
+    elseif loggedalready == true and not IsPlayerAceAllowed(source, 'afk:immune') then
+        DropPlayer(source, reason)
+    end
+end)
+
+function DiscordWebHook(name, message, color)
+    local connect = {
+          {
+              ["color"] = color,
+              ["title"] = "**" .. name .. "**",
+              ["description"] = message,
+              ["footer"] = {
+              ["text"] = footer,
+              },
+          }
+      }
+    PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = GetPlayerName(source), embeds = connect}), { ['Content-Type'] = 'application/json' })
+  end
